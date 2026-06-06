759b4a21932ccfbf39e3257225f14becf37eef2e
croWbar005 <harshilsharma205@gmail.com>
2026-06-07 02:25:46 +0530

    7th question: Reverse String solved

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
-};
+function reverseString(str){
+    str=str.split("");
+    str=str.reverse();
+    str=str.join("");
+    return str;
+}
 
 // Do not edit below this line
 module.exports = reverseString;
