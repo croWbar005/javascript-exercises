0d3a5844d0cc1d68eb401c79f3a0d89a2c68367a
croWbar005 <harshilsharma205@gmail.com>
2026-06-07 01:58:12 +0530

    6th completed

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
-};
-
+function repeatString(str, a) {
+    if(a<0){
+        return "ERROR";
+    }
+    else{
+        let temp="";
+        for (let x=0;x<a;x++){
+            temp+=str;
+        }
+        return temp;
+    }
+}
 // Do not edit below this line
 module.exports = repeatString;
