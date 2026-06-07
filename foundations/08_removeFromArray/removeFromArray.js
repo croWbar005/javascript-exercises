854653ba0e09744b593fd3b47414edb0661bc43a
croWbar005 <harshilsharma205@gmail.com>
2026-06-08 02:03:19 +0530

    8th question solved

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-};
-
+function removeFromArray(arr,...a) {
+    for(let x=0;x<=a.length;x++){
+        arr= arr.filter(num => num!==a[x]);
+    }
+    return arr;       
+}
+ console.log(removeFromArray([1,2,3,4,5,6],"1",3));
 // Do not edit below this line
 module.exports = removeFromArray;
